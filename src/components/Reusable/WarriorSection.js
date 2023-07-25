c1e2d34da28764903154b957d12199e5a1dd2653
Narutocr7 <bhavishya.pachika@gmail.com>
2023-07-25 08:42:51 +0530

    Your commit msg here

--- src/components/Reusable/WarriorSection.js
+++ src/components/Reusable/WarriorSection.js
@@ -1,16 +1,14 @@
 import React from 'react'
-import BackgroundImage from 'gatsby-background-image'
 
-export default function WarriorSection({img, title, subtitle, heroclass}) {
+export default function WarriorSection({title, subtitle, heroclass}) {
   return (
-    <BackgroundImage
+    <div
     className={heroclass}
-    fluid={img}
     >
         <h1 className="text-white text-uppercase text-center display-4">{title}</h1>
         <div>
             <h4 className="text-warning">{subtitle}</h4>
         </div>
-    </BackgroundImage>
+    </div>
   )
 }
